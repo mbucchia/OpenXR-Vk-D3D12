3256ab01ef5d6b92581dfeda4dca9d89b24ae60c
Matthieu Bucchianeri <mbucchia@gmail.com>
2023-09-01 13:31:35 -0700

    Bump version to 1.1.0.

--- XR_APILAYER_MBUCCHIA_vulkan_d3d12_interop/version.h
+++ XR_APILAYER_MBUCCHIA_vulkan_d3d12_interop/version.h
@@ -1,3 +1,3 @@
 const unsigned int LayerVersionMajor = 1;
-const unsigned int LayerVersionMinor = 0;
-const unsigned int LayerVersionPatch = 1;
+const unsigned int LayerVersionMinor = 1;
+const unsigned int LayerVersionPatch = 0;
